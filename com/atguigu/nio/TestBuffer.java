d6fa02da7b13095d076771021376c9fde90db511
yanlp <85827444@qq.com>
2020-04-19 10:48:30 +0800

    直接缓冲区和非直接缓冲区

--- com/atguigu/nio/TestBuffer.java
+++ com/atguigu/nio/TestBuffer.java
@@ -6,7 +6,7 @@ import java.nio.ByteBuffer;
 
 /**
  * 一、缓冲区（Buffer）： 在  Java NIO 中主要负责数据的存取。缓冲区实际上就是数组。缓冲区可以存储不同类型的数据
- * <p>
+ * 
  * 根据数据类型不同(boolean 除外)，提供了对应类型的缓冲区：
  * ByteBuffer
  * CharBuffer
@@ -15,21 +15,26 @@ import java.nio.ByteBuffer;
  * LongBuffer
  * FloatBuffer
  * DoubleBuffer
- * <p>
+ * 
  * 上述缓冲区的管理方式几乎一致，通过 allocate() 方法分配一个缓冲区。
- * <p>
+ * 
  * 二、缓冲区存取数据的核心方法：
  * put()
  * get()
- * <p>
+ * 
  * 三、缓冲区的四大核心属性：
  * capacity : 容量，缓冲区中存储数据的容量。一旦指定不能改变。
  * limit : 界限，缓冲区中可以操作数据的大小。（limit 后的数据不能进行读写）
  * position : 位置，缓冲区中当前正在操作数据的位置。
- * <p>
+ * 
  * mark : 标记，记录当前 position 的位置，通过 reset() 方法恢复到之前 mark 的位置
- * <p>
+ * 
  * 0 <= mark <= position <= limit <= capacity
+ *
+ * 四.直接缓冲区和非直接缓冲区
+ * 非直接缓冲区: 通过 allocate()方法,将缓冲区建立在JVM内存中
+ * 直接缓冲区(ByteBuffer 特有):通过 allocateDirect()方法,将缓冲区建立在操作系统的物理内存中
+ *
  * Program Name: nio
  * Created by yanlp on 2020-04-19
  *
@@ -37,6 +42,13 @@ import java.nio.ByteBuffer;
  * @version 1.0
  */
 public class TestBuffer {
+
+    @Test
+    public void test3() {
+        ByteBuffer buf = ByteBuffer.allocateDirect(1024);
+
+        System.out.println(buf.isDirect());
+    }
 
     @Test
     public void test2() {
